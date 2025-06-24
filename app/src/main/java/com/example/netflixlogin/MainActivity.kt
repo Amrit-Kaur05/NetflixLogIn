7b7df93448e6bed42900c307eb11fb0cecd7de4e
Amrit-Kaur05 <129068474+Amrit-Kaur05@users.noreply.github.com>
2025-06-24 16:46:25 -0400

    Adding to interface

--- app/src/main/java/com/example/netflixlogin/MainActivity.kt
+++ app/src/main/java/com/example/netflixlogin/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.netflixlogin
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var birdPicture: ImageView
+    private lateinit var puppyPicture: ImageView
+    private lateinit var fishPicture: ImageView
+    private lateinit var kittyPicture: ImageView
+    private lateinit var kidsPicture: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,25 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        configureViews()
+        configureImage()
     }
+
+    private fun configureViews() {
+        birdPicture = findViewById(R.id.birdImg)
+        puppyPicture = findViewById(R.id.puppyImg)
+        fishPicture = findViewById(R.id.fishImg)
+        kittyPicture = findViewById(R.id.kittyImg)
+        kidsPicture = findViewById(R.id.kidsImg)
+    }
+
+    private fun configureImage() {
+        birdPicture.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.bird))
+        puppyPicture.setImageDrawable(AppCompatResources.getDrawable(this,R.drawable.puppy))
+        fishPicture.setImageDrawable(AppCompatResources.getDrawable(this,R.drawable.goldfish))
+        kittyPicture.setImageDrawable(AppCompatResources.getDrawable(this,R.drawable.kitty))
+        kidsPicture.setImageDrawable(AppCompatResources.getDrawable(this,R.drawable.babyanimals))
+    }
+
 }
